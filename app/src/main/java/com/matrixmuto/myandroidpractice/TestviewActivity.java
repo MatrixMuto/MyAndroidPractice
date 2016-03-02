f70244f91f16f52e6aa116875d862bc64de0fb61
MatrixMuto <wang.qiang.mu@gmail.com>
2016-03-02 16:07:53 +0800

    add testview

---
+++ app/src/main/java/com/matrixmuto/myandroidpractice/TestviewActivity.java
@@ -0,0 +1,17 @@
+package com.matrixmuto.myandroidpractice;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class TestviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testview);
+    }
+
+}
